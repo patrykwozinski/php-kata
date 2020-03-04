eef51bd66865ff7e6766141498223d72444b2761
Patryk Woziński <patryk.wozinski@gmail.com>
2020-03-04 14:21:21 +0100

    for bank

---
+++ src/example.php
@@ -0,0 +1,9 @@
+<?php
+
+include_once __DIR__ . '/../vendor/autoload.php';
+
+$account = null;
+//$account->deposit(500);
+//$account->deposit(300);
+//$account->withdraw(100);
+//$account->printStatement();
